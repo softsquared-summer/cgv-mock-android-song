923a513562e1259309a85416d8b3b20f87c1d0a4
sdy1423 <53420624+sdy1423@users.noreply.github.com>
2020-02-14 20:11:49 +0900

    Viewpager+Tablayout 구현
    Viewpager+Tablayout 으로 상단탭 구현.

--- MOCK_CGV/app/src/main/java/com/example/mock_cgv/src/main/MainActivity.java
+++ MOCK_CGV/app/src/main/java/com/example/mock_cgv/src/main/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mock_cgv.src.main;
 
 import androidx.annotation.Nullable;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -10,20 +11,31 @@ import android.widget.TextView;
 import com.example.mock_cgv.R;
 import com.example.mock_cgv.src.BaseActivity;
 import com.example.mock_cgv.src.main.interfaces.MainActivityView;
+import com.google.android.material.tabs.TabLayout;
 
 import static com.example.mock_cgv.src.ApplicationClass.X_ACCESS_TOKEN;
 import static com.example.mock_cgv.src.ApplicationClass.sSharedPreferences;
 
 public class MainActivity extends BaseActivity implements MainActivityView {
-    private TextView mTvHelloWorld; //안드로이드 지정 변수명 가이드라인
+    //private TextView mTvHelloWorld; //안드로이드 지정 변수명 가이드라인
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTvHelloWorld = findViewById(R.id.main_tv_hello_world);
 
+        mTabLayout=findViewById(R.id.main_tl_tabLayout);
+        mViewPager=findViewById(R.id.main_vp_viewPager);
+
+        MainViewPagerAdapter mainViewPagerAdapter = new MainViewPagerAdapter(getSupportFragmentManager(),mTabLayout.getTabCount());
+        mViewPager.setAdapter(mainViewPagerAdapter);
+
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+        mTabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
     }
+
     private void tryGetTest() {
         showProgressDialog();
 
@@ -34,33 +46,28 @@ public class MainActivity extends BaseActivity implements MainActivityView {
     @Override
     public void validateSuccess(String text) {
         hideProgressDialog();
-        mTvHelloWorld.setText(text);
-
-        SharedPreferences.Editor a = sSharedPreferences.edit();
-        a.putString(X_ACCESS_TOKEN,text);
-        a.apply();
 
     }
+
     @Override
     public void validateSuccess201(String text) {
         hideProgressDialog();
-        mTvHelloWorld.setText("201"+text); //mainserviec에서 건드리지 말고 인터페이스 만들고 액티비티에서 바꿔라
     }
 
     @Override
     public void validateFailure(@Nullable String message) {
         hideProgressDialog();
-        showCustomToast(message ==null || message.isEmpty() ? getString(R.string.network_error):message);
+        showCustomToast(message == null || message.isEmpty() ? getString(R.string.network_error) : message);
     }
 
-
-    public void customOnClick(View view){
-        switch (view.getId()){
-            case R.id.main_btn_hello_world:
-                tryGetTest();
-                break;
-        }
-    }
+//
+//    public void customOnClick(View view) {
+//        switch (view.getId()) {
+//            case R.id.main_btn_hello_world:
+//                tryGetTest();
+//                break;
+//        }
+//    }
 
 
 }
