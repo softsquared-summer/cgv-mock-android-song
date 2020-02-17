9abfdd0d548af4579ccc799b9b155bbfe19c85f2
sdy1423 <53420624+sdy1423@users.noreply.github.com>
2020-02-18 00:08:45 +0900

    우측 메뉴,custom toolbar 구현
    drawerlayout 사용해서 우측 메뉴 구현함, 기존 actionbar없애고 toolbar 구현

--- MOCK_CGV/app/src/main/java/com/example/mock_cgv/src/main/event/AutoScrollAdapter.java
+++ MOCK_CGV/app/src/main/java/com/example/mock_cgv/src/main/event/AutoScrollAdapter.java
@@ -24,10 +24,11 @@ public class AutoScrollAdapter extends InfinitePagerAdapter {
     private List<String> mData;
     private Context mContext;
 
-    public AutoScrollAdapter(List<String> data,Context context){
-        this.mData=data;
-        this.mContext=context;
+    public AutoScrollAdapter(List<String> data, Context context) {
+        this.mData = data;
+        this.mContext = context;
     }
+
     @Override
     public int getItemCount() {
         return mData == null ? 0 : mData.size();
@@ -35,49 +36,16 @@ public class AutoScrollAdapter extends InfinitePagerAdapter {
 
     @Override
     public View getItemView(int position, View convertView, ViewGroup container) {
-        ((ViewGroup)container.getParent()).removeView(convertView);
+        ((ViewGroup) container.getParent()).removeView(convertView);
 
         LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        convertView = inflater.inflate(R.layout.event_auto_viewpager,null);
+        convertView = inflater.inflate(R.layout.event_auto_viewpager, null);
         ImageView image_container = (ImageView) convertView.findViewById(R.id.image_container);
         Glide.with(mContext).load(mData.get(position)).into(image_container);
-        ((ViewGroup)container.getParent()).removeView(convertView);
+        ((ViewGroup) container.getParent()).removeView(convertView);
         container.addView(convertView);
         container.removeView(convertView);
 
         return convertView;
     }
-//    Context mContext;
-//    ArrayList<String> mData;
-//
-//    public AutoScrollAdapter(Context context,ArrayList<String> data){
-//        this.mContext=context;
-//        this.mData=data;
-//    }
-//
-//    @NonNull
-//    @Override
-//    public Object instantiateItem(@NonNull ViewGroup container, int position) {
-//        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//        View v = inflater.inflate(R.layout.event_auto_viewpager,null);
-//        ImageView image_container = (ImageView) v.findViewById(R.id.image_container);
-//        Glide.with(mContext).load(mData.get(position)).into(image_container);
-//        container.addView(v);
-//        return v;
-//    }
-//
-//    @Override
-//    public int getCount() {
-//        return mData.size();
-//    }
-//
-//    @Override
-//    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-//        container.removeView((View)object);
-//    }
-//
-//    @Override
-//    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-//        return view == object;
-//    }
 }
